42121cd94d9dd00eb24d7864625a2c24867a38fd
Scott <sdmccoy@gmail.com>
2017-11-27 15:41:45 -0800

    hello world is up

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+
+
+class App extends React.Component{
+
+  render(){
+    return(
+      <div className='app'>
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
 import App from './component/app';
 
-ReactDOM.render((
-  <BrowserRouter>
+ReactDOM.render(
+  (<BrowserRouter>
     <Route exact path='*' component={App} />
-  </BrowserRouter>), document.getElementById('root'));
+  </BrowserRouter>), document.getElementById('root')
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
